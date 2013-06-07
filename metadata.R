ada4978ab3487414b31446c2075b0fb74a855518
Andrew Goldstone <andrew.goldstone@gmail.com>
2013-06-07 14:36:44 -0400

    read_metadata, pubdate_Date

--- metadata.R
+++ metadata.R
@@ -1,30 +1,41 @@
 # examples of elementary manipulation using the metadata
-# this file is unfinished notes and sketches, not a usable script.
 
-# read.citations: make a dataframe from citations.CSV
-# filename: source citations.csv file
-# strip: determines whether read.csv will strip leading and trailing
-# whitespace from each field. NB jstor puts a trailing tab at the end of each
-# entry in fields that can contain multiple tab-separated entries (e.g. author)
+# read_metadata: make a dataframe from citations.CSV
+#
+# filename: source citations.CSV file
+#
+# assumes that the file has a trailing comma at the end of every line,
+# which makes read.csv find an extra field. DfR has changed their output
+# data format before, so check results carefully.
+#
+# Any extra parameters are passed on to read.csv; the most useful may
+# be strip.white, which determines whether read.csv will strip leading
+# and trailing whitespace from each field. NB jstor puts a trailing
+# tab at the end of each entry in fields that can contain multiple
+# tab-separated entries (e.g. author)--even if there is only a single
+# entry
 
-read.citations <- function(filename=NA,strip=FALSE) { 
-    # Let's hardcode the headers, since read.csv gets confused by
-    # the trailing comma at the end of every line
 
-    cols <- c("id","doi","title","author","journaltitle","volume","issue","pubdate","pagerange","publisher","type","reviewed.work","unused") 
-
-    citations.filename <- filename
+read_metadata <- function(filename=NA,...) { 
+    f <- filename
     if(is.na(filename)) { 
-        cat(
-  "Select citations.CSV file from jstor dfr...\n")
+        cat("Select citations.CSV file from jstor dfr...\n")
         ignore <- readline("(press return to open file dialog) ")
-        citations.filename <- file.choose()
-        print(citations.filename)
+        f <- file.choose()
+        print(f)
     }
 
-    read.csv(citations.filename,strip.white=strip,skip=1,
-             col.names=cols,header=FALSE,as.is=TRUE)
+    # the nefarious trailing comma:
+    cols <- scan(f,nlines=1,what=character(),sep=",",quiet=T)
+    cols <- c(cols,"unused")
+
+    subset(read.csv(f,skip=1,header=F,col.names=cols,as.is=T,...),
+           select=-unused)
 }
+    
+# legacy function: now just a wrapper around read_metadata
+
+read.citations <- function(filename=NA,strip=FALSE) { read_metadata(filename,strip.white=strip) }
 
 # given a dataframe as returned by read.citations,
 # return a dataframe with only full length articles 
@@ -80,6 +91,12 @@ pubdate.to.years <- function(datestrs) {
 daterange.subset <- function(df,min.incl,max.excl) {
     subset(df,pubdate.to.years(pubdate) >= min.incl
         & pubdate.to.years(pubdate) < max.excl)
+}
+
+# to keep month and day information, use this instead, which returns
+# Date object(s)
+pubdate_Date <- function(pubdate) {
+    as.Date(substr(pubdate,1,10))
 }
 
 # dump all the metadata in a dataframe into a sqlite database in case
