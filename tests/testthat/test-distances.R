7f72877ab225c20a700ef025ca2e12e8091ba331
Andrew Goldstone <andrew.goldstone@gmail.com>
2013-12-08 11:50:29 -0500

    a test for my JS_divergence formula

--- tests/testthat/test-distances.R
+++ tests/testthat/test-distances.R
@@ -2,17 +2,25 @@
 # KLdiv. Not faster on a single pair of rows, and can't use KLdiv's
 # vectorization to do lots of JS's at once, unfortunately.
 
-testthat("JS works right", {
-    JS_flexmix <- function(P,Q) {
-        library(flexmix)
-        PQ_mean = (P + Q) / 2
-        eps = min(P,Q) / 2    # otherwise KLdiv replaces values less than 10^-4
-        result <- (KLdiv(cbind(P,PQ_mean),eps=eps) +
-                   KLdiv(cbind(Q,PQ_mean),eps=eps)) / 2
-        result[1,2]
-    }
+test_that("JS_divergence works right", {
+    if(require("flexmix")) {
+        JS_flexmix <- function(P,Q) {
+            PQ_mean = (P + Q) / 2
+            # eps small, otherwise KLdiv replaces values less than 10^-4
+            eps = min(P,Q) / 2    
+            result <- (KLdiv(cbind(P,PQ_mean),eps=eps) +
+                       KLdiv(cbind(Q,PQ_mean),eps=eps)) / 2
+            result[1,2]
+        }
 
-    # P <- 
-    # Q <-
-    expect_that(JS_flexmix(P,Q),equals(JS_divergence(P,Q)))
+        # I *think* this is equivalent to taking two single draws
+        # from a Dirichlet distribution with K = 100, sum alpha = 5
+        P <- rgamma(100,5)
+        P <- P / sum(P)
+        Q <- rgamma(100,5)
+        Q <- Q / sum(Q)
+        expect_that(JS_flexmix(P,Q),equals(JS_divergence(P,Q)))
+    } else {
+        message("flexmix library not available. Skipping test")
+    }
 })
