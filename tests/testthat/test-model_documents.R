d37b0592e461a872ae8a678204b8c1955a7dce4f
Andrew Goldstone <andrew.goldstone@gmail.com>
2015-09-09 08:46:22 -0400

    rewritten modeling tests
    sanity-checking on small set, then model_documents on whole test data
    folder

--- tests/testthat/test-model_documents.R
+++ tests/testthat/test-model_documents.R
@@ -1,48 +1,52 @@
+context("Modeling")
+
+data_dir <- file.path(path.package("dfrtopics"),
+                      "test-data", "pmla-modphil1905-1915")
+
+# we're just cross-checking functions here, so use an arbitrary
+# subset of the sample data
+
+fs <- sample(list.files(file.path(data_dir, "wordcounts"), full.names=T),
+             60)
+
+stoplist_file <- file.path(path.package("dfrtopics"), "stoplist",
+                           "stoplist.txt")
+
+n_topics <- 12 # topics
+n_top_words <- 10 # key words
+
 test_that("Modeling on some sample data works", {
     
-    data_dir <- file.path(path.package("dfrtopics"),"dfr_data")
+    m <- list()
+    m$metadata <- read_metadata(file.path(data_dir, "citations.tsv"))
+    insts <- make_instances(read_dfr_wordcounts(files=fs),
+                            stoplist_file=stoplist_file)
 
-    # check for presence of sample data
+    m$trainer <- train_model(insts,
+                             n_topics=n_topics,
+                             n_iters=200,
+                             threads=1, 
+                             alpha_sum=5,
+                             beta=0.01,
+                             n_hyper_iters=20,
+                             n_burn_in=20,
+                             n_max_iters=10)
 
-    expect_that(file.exists(data_dir),is_true())
-    expect_that(file.exists(file.path(data_dir,"citations.CSV")),is_true())
-    expect_that(length(list.files(file.path(data_dir,"wordcounts"))),
-                equals(60))
+    expect_equal(class(m$trainer), "jobjRef", check.attributes=F)
+    expect_equal(.jstrVal(m$trainer$getClass()),
+                 "class cc.mallet.topics.RTopicModel")
 
-    # run model
-
-    n <- 8 # topics
-    n_top_words <- 10 # key words
-
-    # the modeling run: we'll also check for valid messaging
-    expect_message((
-    m <- model_documents(citations_files=file.path(data_dir,"citations.CSV"),
-                         dirs=file.path(data_dir,"wordcounts"),
-                         stoplist_file=file.path(path.package("dfrtopics"),
-                                                 "stoplist","stoplist.txt"),
-                         n_topics=n,
-                         n_iters=140,
-                         seed=42,
-                         threads=1L, 
-                         alpha_sum=5,beta=0.01,
-                         n_hyper_iters=20,
-                         n_burn_in=20,
-                         n_max_iters=0,
-                         n_top_words=n_top_words)
-    ), "MALLET random number seed set to 42")
-
-    # remembered the seed?
-    expect_that(m$seed,equals(42))
+    m$wkf <- weighted_keys_frame(m$trainer, n_top_words)
+    m$doc_topics <- doc_topics_frame(m$trainer)
 
     # right number of rows in doc-topics?
-    expect_that(nrow(m$doc_topics),
-                equals(length(list.files(file.path(data_dir,"wordcounts")))))
+    expect_that(nrow(m$doc_topics), equals(length(fs)))
 
     # right number of cols?
-    expect_that(ncol(m$doc_topics),equals(n + 1))
+    expect_that(ncol(m$doc_topics), equals(n_topics + 1))
 
     # right number of key words in keys frame?
-    expect_that(nrow(m$wkf),equals(n * n_top_words))
+    expect_that(nrow(m$wkf), equals(n_topics * n_top_words))
 
 
     # check that file output creates all the expected files
@@ -73,9 +77,9 @@ test_that("Modeling on some sample data works", {
 
     clear_files(out_files)
 
-    output_model(m,out_dir,save_instances=T,save_scaled=T)
+    output_model(m, out_dir, save_instances=T, save_scaled=T)
 
-    expect_that(all(sapply(out_files,file.exists)),is_true())
+    expect_that(all(sapply(out_files,file.exists)), is_true())
 
     clear_files(out_files)
 
@@ -84,9 +88,39 @@ test_that("Modeling on some sample data works", {
     m$wkf <- NULL
     m$doc_topics <- NULL
 
-    output_model(m,out_dir,save_instances=T,save_scaled=T)
+    output_model(m, out_dir, save_instances=T, save_scaled=T)
 
-    expect_that(all(sapply(out_files,file.exists)),is_true())
+    expect_that(all(sapply(out_files, file.exists)), is_true())
 
     clear_files(out_files)
+
 }) 
+
+test_that("Convenience function model_documents completes", {
+    # the modeling run: we'll also check for valid messaging
+    expect_message(
+        (
+            m <- model_documents(file.path(data_dir, "citations.tsv"),
+                                 file.path(data_dir, "wordcounts"),
+                                 stoplist_file,
+                                 n_topics=n_topics,
+                                 seed=42,
+                                 n_top_words=n_top_words)
+        ),
+        "MALLET random number seed set to 42"
+    )
+
+
+    # remembered the seed?
+    expect_that(m$seed, equals(42))
+
+    # right number of rows in doc-topics?
+    expect_that(nrow(m$doc_topics),
+                equals(length(list.files(file.path(data_dir, "wordcounts")))))
+
+    # right number of cols?
+    expect_that(ncol(m$doc_topics), equals(n_topics + 1))
+
+    # right number of key words in keys frame?
+    expect_that(nrow(m$wkf), equals(n_topics * n_top_words))
+})
