841e5277ee7cd969c97ee4340482960b001cc555
Andrew Goldstone <andrew.goldstone@gmail.com>
2015-09-14 11:52:20 -0400

    comments reflow

--- R/sampling_state.R
+++ R/sampling_state.R
@@ -4,8 +4,8 @@
 
 #' Save the Gibbs sampling state to a file
 #'
-#' Saves the MALLET sampling state using MALLET's own state-output routine, which produces 
-#' a ginormous gzipped textfile.
+#' Saves the MALLET sampling state using MALLET's own state-output routine,
+#' which produces a ginormous gzipped textfile.
 #'
 #' @param m the \code{dfr_lda} model object
 #' @param outfile the output file name
@@ -25,16 +25,19 @@ write_mallet_state <- function(m, outfile="state.gz") {
 
 #' Reduce a MALLET sampling state on disk to a simplified form
 #'
-#' This function reads in the sampling state output by MALLET and writes a CSV file 
-#' giving the assignments of word types to topics in each document.
-#' Because the MALLET state file 
-#' is often too big to handle in memory using R, the "simplification" is done 
-#' using a very simple python script instead. 
-#' 
-#' The resulting file has a header \code{document,word,topic,count} describing its 
-#' columns. Use \code{\link{read_sampling_state}} to access the result in R if it is too large to handle in memory. (If the file is big but fits in memory, I recommend \code{read_csv} from the \code{readr} package, which will yield an ordinary data frame.)
+#' This function reads in the sampling state output by MALLET and writes a CSV
+#' file giving the assignments of word types to topics in each document. Because
+#' the MALLET state file is often too big to handle in memory using R, the
+#' "simplification" is done using a very simple python script instead.
 #'
-#' Note that this file uses zero-based indices for topics, words, and documents, not 1-based indices.
+#' The resulting file has a header \code{document,word,topic,count} describing
+#' its columns. Use \code{\link{read_sampling_state}} to access the result in R
+#' if it is too large to handle in memory. (If the file is big but fits in
+#' memory, I recommend \code{read_csv} from the \code{readr} package, which will
+#' yield an ordinary data frame.)
+#'
+#' Note that this file uses zero-based indices for topics, words, and documents,
+#' not 1-based indices.
 #'
 #'
 #' @param state_file the MALLET state (assumed to be gzipped)
@@ -61,39 +64,40 @@ simplify_state <- function (state_file, outfile) {
 #'
 #' This function reads in a Gibbs sampling state represented by
 #' \code{document,word,topic,count} rows to a
-#' \code{\link[bigmemory]{big.matrix}}. This gives the model's assignments of words
-#' to topics within documents. 
-#' MALLET itself remembers token order, but this is meaningless when
-#' working with JSTOR wordcounts file, and more generally in ordinary LDA the words are assumed exchangeable within documents.
+#' \code{\link[bigmemory]{big.matrix}}. This gives the model's assignments of
+#' words to topics within documents. MALLET itself remembers token order, but
+#' this is meaningless when working with JSTOR wordcounts file, and more
+#' generally in ordinary LDA the words are assumed exchangeable within
+#' documents.
 #'
 #'
-#' for reading MALLET \code{InstanceList}s: \code{\link{instances_vocabulary}} and 
-#' \code{\link{instances_ids}}. \emph{N.B.} the MALLET sampling state, and the "simplified 
-#' state" output by this function to disk, index documents, words, and topics from zero, 
-#' but the dataframe returned by this function indexes these from one for convenience 
-#' within R.
+#' for reading MALLET \code{InstanceList}s: \code{\link{instances_vocabulary}}
+#' and \code{\link{instances_ids}}. \emph{N.B.} the MALLET sampling state, and
+#' the "simplified state" output by this function to disk, index documents,
+#' words, and topics from zero, but the dataframe returned by this function
+#' indexes these from one for convenience within R.
 #'
-#' @return a \code{big.matrix} with four columns, 
-#' \code{document,word,topic,count}. Documents, words, and topics are \emph{one-indexed} 
-#' in the result, so these values may be used as indices to the vectors returned by \code{\link{doc_ids}}, \code{\link{vocabulary}}, \code{\link{doc_topics}}, etc.
+#' @return a \code{big.matrix} with four columns,
+#'   \code{document,word,topic,count}. Documents, words, and topics are
+#'   \emph{one-indexed} in the result, so these values may be used as indices to
+#'   the vectors returned by \code{\link{doc_ids}}, \code{\link{vocabulary}},
+#'   \code{\link{doc_topics}}, etc.
 #'
-#' @param filename the name of a CSV file holding the simplified state: a CSV with header 
-#' row and four columns, \code{document,word,topic,count}, where the documents, words, and 
-#' topics are \emph{zero-index}. Create the file from MALLET output using 
-#' \code{\link{simplify_state}}.
-#' @param data_type the C++ type to store the data in. If all values have magnitude 
-#' less than \eqn{2^15}, you can get away with \code{"short"}, but guess what? Linguistic
-#' data hates you, and a typical vocabulary can easily include more word types than that, so the default is \code{"integer"}.
-#' @param big_wordkir the working directory where \code{\link[bigmemory]{read.big.matrix}} 
-#' will store its temporary files. By default, uses \code{\link[base]{tempdir}}, but if 
-#' you 
-#' have more scratch space elsewhere, use that for handling large sampling states.
+#' @param filename the name of a CSV file holding the simplified state: a CSV
+#'   with header row and four columns, \code{document,word,topic,count}, where
+#'   the documents, words, and topics are \emph{zero-index}. Create the file
+#'   from MALLET output using \code{\link{simplify_state}}.
+#' @param data_type the C++ type to store the data in. If all values have
+#'   magnitude less than \eqn{2^15}, you can get away with \code{"short"}, but
+#'   guess what? Linguistic data hates you, and a typical vocabulary can easily
+#'   include more word types than that, so the default is \code{"integer"}.
+#' @param big_wordkir the working directory where
+#'   \code{\link[bigmemory]{read.big.matrix}} will store its temporary files. By
+#'   default, uses \code{\link[base]{tempdir}}, but if you have more scratch
+#'   space elsewhere, use that for handling large sampling states.
 #'
-#' @seealso
-#' \code{\link{simplify_state}},
-#' \code{\link{write_mallet_state}},
-#' \code{\link{term_year_topic_matrix}},
-#' \pkg{bigmemory}
+#' @seealso \code{\link{simplify_state}}, \code{\link{write_mallet_state}},
+#' \code{\link{term_year_topic_matrix}}, \pkg{bigmemory}
 #'
 #' @export
 #'
@@ -145,15 +149,20 @@ m <- load_state(m)'
 
 #' Load Gibbs sampling state into model object
 #'
-#' This is a convenience function for loading the sampling state into a model 
+#' This is a convenience function for loading the sampling state into a model
 #' object. If no file names are supplied, the state information will be written
 #' to (possibly very large) temporary files before being read back into memory.
 #'
 #' @param m \code{dfr_lda} object
-#' @param simplified_state_file Name of simplified state file (from \code{\link{simplify_state}}. If NULL, a temporary file will be created
-#' @param mallet_state_file Name of file with mallet's own gzipped sampling-state output (from \code{\link{write_mallet_state}} or command-line mallet). If NULL, the state will be written to a temporary file
+#' @param simplified_state_file Name of simplified state file (from
+#'   \code{\link{simplify_state}}. If NULL, a temporary file will be created
+#' @param mallet_state_file Name of file with mallet's own gzipped
+#'   sampling-state output (from \code{\link{write_mallet_state}} or
+#'   command-line mallet). If NULL, the state will be written to a temporary
+#'   file
 #'
-#' @return a copy \code{m} with the sampling state loaded (available via \code{sampling_state(m)}
+#' @return a copy \code{m} with the sampling state loaded (available via
+#'   \code{sampling_state(m)}
 #'
 #' @export
 #'
@@ -192,22 +201,29 @@ load_sampling_state <- function (m,
 
 #' The term-document matrix for a topic
 #'
-#' Extracts a matrix of counts of words assigned to a given topic in each document from the model's final Gibbs sampling state.
+#' Extracts a matrix of counts of words assigned to a given topic in each
+#' document from the model's final Gibbs sampling state.
 #'
-#' This is useful for studying a topic conditional on some metadata covariate: it is important to realize that frequent words in the overall topic distribution may not be the same as very frequent words in that distribution over some sub-group of documents, particularly if the corpus contains widely varying language use. If, for example, the corpus stretches over a long time period, consider comparing the early and late parts of each of the within-topic term-document matrices.
+#' This is useful for studying a topic conditional on some metadata covariate:
+#' it is important to realize that frequent words in the overall topic
+#' distribution may not be the same as very frequent words in that distribution
+#' over some sub-group of documents, particularly if the corpus contains widely
+#' varying language use. If, for example, the corpus stretches over a long time
+#' period, consider comparing the early and late parts of each of the
+#' within-topic term-document matrices.
 #'
-#' @return a \code{\link[Matrix]{sparseMatrix}} of \emph{within-topic} word weights (unsmoothed and unnormalized) with terms in rows and documents in columns (same ordering as \code{vocabulary(m)} and \code{doc_ids(m)})
+#' @return a \code{\link[Matrix]{sparseMatrix}} of \emph{within-topic} word
+#'   weights (unsmoothed and unnormalized) with terms in rows and documents in
+#'   columns (same ordering as \code{vocabulary(m)} and \code{doc_ids(m)})
 #'
 #' @param m a \code{dfr_lda} object with the sampling state loaded
-#' \code{\link{read_sampling_state}}. Operated on using \code{\link[bigmemory]{mwhich}}.
+#'   \code{\link{read_sampling_state}}. Operated on using
+#'   \code{\link[bigmemory]{mwhich}}.
 #'
-#' @seealso \code{\link{read_sampling_state}},
-#' \code{\link{dfr_lda}},
-#' \code{\link{load_sampling_state}},
-#' \code{\link{top_n_row}},
-#' \code{\link{sum_col_groups}},
-#' \code{\link{term_year_matrix}}
-#' 
+#' @seealso \code{\link{read_sampling_state}}, \code{\link{dfr_lda}},
+#'   \code{\link{load_sampling_state}}, \code{\link{top_n_row}},
+#'   \code{\link{sum_col_groups}}, \code{\link{term_year_matrix}}
+#'
 #' @export
 #'
 tdm_topic <- function (m, topic) {
