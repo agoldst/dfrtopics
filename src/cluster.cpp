b319c40f107ad1a5507ac073fe4cf4e49efd93b0
Andrew Goldstone <andrew.goldstone@gmail.com>
2016-04-20 13:59:15 -0400

    CRASHING still. flag for naive_cluster logging
    and whitespace cleanup

--- src/cluster.cpp
+++ src/cluster.cpp
@@ -26,13 +26,13 @@ IntegerVector naive_cluster(NumericVector D, int M, int K,
     if (M * (M - 1) / 2 * K * K != D.size()) {
         stop("The length of D is not consistent with M and K");
     }
-    
+
     // initialization
     std::iota(result.begin(), result.end(), 0);
-    for (int i = 0; i < K * M; ++i) { 
+    for (int i = 0; i < K * M; ++i) {
         clusters[i].insert(i);
     }
-    
+
     int d = 0; // runs along D
     for (int i = 0; i < (M - 1) * K; ++i) {
 
@@ -41,13 +41,13 @@ IntegerVector naive_cluster(NumericVector D, int M, int K,
         // along a block-lower-triangular matrix whose (I, J) block is the
         // K x K matrix of distances between topics in models I and J. And
         // blocks are omitted.
-        // 1.1:2.1 1.1:2.2 ... 1.1:M.K 
+        // 1.1:2.1 1.1:2.2 ... 1.1:M.K
         // 1.2:2.1 1.1:2.1 ... 1.2:M.K
         // ...
         // 2.1:3.1 ...
         // ...
         // (M - 1).1:M.1 ... (M - 1):M.K
-        // where a.b:c.d = distance from topic a in model b to topic c in 
+        // where a.b:c.d = distance from topic a in model b to topic c in
         // model d.
         for (int j = i / K + K; j < M * K; ++j) {
             dst[d].d = D[d]; // copying D like space is cheap or something
@@ -56,7 +56,7 @@ IntegerVector naive_cluster(NumericVector D, int M, int K,
             d += 1;
         }
     }
-    
+
     pair_dist_cmp pdc;
     std::sort(dst.begin(), dst.end(), pdc);
 
@@ -68,17 +68,19 @@ IntegerVector naive_cluster(NumericVector D, int M, int K,
         if (d->d > threshold) { // then we're done
             break;
         }
-        // topic pair's positions in the sequence of all topics 
+        // topic pair's positions in the sequence of all topics
         d->i = d->i;
         d->j = d->j;  // guaranteed to be > d->i
         if (d->i >= d->j) {
             stop("Something's wrong: d->i >= d->j");
         }
+
+#ifdef __NAIVE_CLUSTER_LOG__
         Rcout << "Consider: " << d->i << " (" << d->i / K << " ";
         Rcout << d->i % K << ") ";
         Rcout << d->j << " (" << d->j / K << " ";
         Rcout << d->j % K << ")";
-
+#endif
         r1 = result[d->i];
         r2 = result[d->j];
         std::set<int> &c1 = clusters[r1];
@@ -86,30 +88,44 @@ IntegerVector naive_cluster(NumericVector D, int M, int K,
 
         if (c1.size() == M || c2.size() == M) {
             // if either cluster is full, merge is impossible
+#ifdef __NAIVE_CLUSTER_LOG__
             Rcout << "...cluster full." << std::endl;
+#endif
             continue;
         }
         // Check whether the two clusters share topics from the same model
         sect.clear();
         allow = true;
+#ifdef __NAIVE_CLUSTER_LOG__
         Rcout << " c1:";
+#endif
         for (std::set<int>::iterator t = c1.begin(); t != c1.end(); ++t) {
+#ifdef __NAIVE_CLUSTER_LOG__
             Rcout <<  " " << *t / K;
+#endif
             sect.insert(*t / K);
-        } 
+        }
+#ifdef __NAIVE_CLUSTER_LOG__
         Rcout << " c2:";
+#endif
         for (std::set<int>::iterator t = c2.begin();
                     t != c2.end() && allow; ++t) {
             // if same model: cluster disallowed
+#ifdef __NAIVE_CLUSTER_LOG__
             Rcout << " " << *t / K;
+#endif
             allow = (sect.count(*t / K) == 0);
         }
         if (!allow) {
+#ifdef __NAIVE_CLUSTER_LOG__
             Rcout << "...disallowed." << std::endl;
+#endif
             continue;
         }
+#ifdef __NAIVE_CLUSTER_LOG__
         Rcout << "...merge." << std::endl;
-        // otherwise: merge 
+#endif
+        // otherwise: merge
         for (std::set<int>::iterator t = c2.begin(); t != c2.end(); ++t) {
             c1.insert(*t);
             result[*t] = r1;
